a36cec2abdb475f20509b77d337e288de620a91b
Pratham Tamrakar <prathamtamrakar849gmail.com>
2025-01-19 13:17:41 +0530

    update file

--- Week-1/assignments/easy/findLargestElement.js
+++ Week-1/assignments/easy/findLargestElement.js
@@ -6,13 +6,13 @@
 */
 
 function findLargestElement(numbers) {
-    let largest = numbers[0];
+    let max = numbers[0];
     for(let i=0;i<numbers.length;i++){
-        if(largest<numbers[i]){
-            largest = numbers[i];
+        if(max<numbers[i]){
+            max = numbers[i];
         }
     }
-    return largest;
+    return max;
 }
 
 module.exports = findLargestElement;

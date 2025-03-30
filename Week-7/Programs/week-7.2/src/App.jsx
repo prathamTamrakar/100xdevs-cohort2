4d52edc16214216aec11891b7673b90b48d42956
Pratham Tamrakar <prathamtamrakar849@gmail.com>
2025-03-30 17:12:14 +0530

    week-7

--- Week-7/Programs/week-7.2/src/App.jsx
+++ Week-7/Programs/week-7.2/src/App.jsx
@@ -1,5 +1,6 @@
-import { useRecoilValue, useRecoilState, RecoilRoot } from 'recoil'
-import { countAtom } from './store/atoms/count'
+import { useRecoilValue, useRecoilState, RecoilRoot, useSetRecoilState } from 'recoil'
+import {countAtom, evenSelector } from './store/atoms/count.jsx'
+import { useMemo } from 'react';
 
 export default function App() {
   return (
@@ -16,12 +17,13 @@ function Count() {
     <div>
       <CountReader />
       <Buttons />
+      <EvenOdd />
     </div>
   )
 }
 
 function CountReader() {
-  const count = useRecoilValue(countAtom)
+  const count = useRecoilValue(countAtom);
   return (
     <div>
       {count}
@@ -30,15 +32,37 @@ function CountReader() {
 }
 
 function Buttons() {
-  const[count, setCount] = useRecoilState(countAtom)
+  // const[count, setCount] = useRecoilState(countAtom)
+  // this component will re-render when the count changes
+  // because it uses the useRecoilState hook
+
+  const setCount  = useSetRecoilState(countAtom)
+  // this component will not re-render when the count changes
+  // setCount(count + 1) === setCount(count => count+1)
   return (
     <div>
       <button onClick={() => {
-        setCount(count + 1)
+        setCount(count => count + 1)
       }} >Increase</button>
       <button onClick={() => {
-        setCount(count - 1)
+        setCount(count => count - 1)
       }}>Decrease</button>
+    </div>
+  )
+}
+
+function EvenOdd(){
+  // const count = useRecoilValue(countAtom);
+  // const isEven = useMemo(()=>{
+  //   return count % 2 === 0
+  // },[count])
+  // selectors in recoil are used to derive state from atoms or other selectors
+
+  const isEven = useRecoilValue(evenSelector)
+
+  return(
+    <div>
+      { isEven ? 'Even' : 'Odd'}
     </div>
   )
 }
--- Week-7/Programs/week-7.2/src/store/atoms/count.jsx
+++ Week-7/Programs/week-7.2/src/store/atoms/count.jsx
@@ -1,6 +1,20 @@
-import {atom} from 'recoil'
+import { atom, selector } from 'recoil'
 
 export const countAtom = atom({
     key: "countAtom",
     default: 0
 })
+
+export const evenSelector = selector({
+    key: 'evenSelector',
+    get: ({ get }) => {
+        const count = get(countAtom)
+        return count % 2 == 0
+    }
+    // explain get , that is 
+})
+
+
+// todo app with filtering logic 
+// webhooks as a service 
+// svix 

8106f57d1911e09c3dc0f3544e3e89592eb5634b
Pratham Tamrakar <prathamtamrakar849@gmail.com>
2025-04-21 03:04:46 +0530

    file updated

--- Week-10/programs/10.1-postgresSQL/dist/index.js
+++ Week-10/programs/10.1-postgresSQL/dist/index.js
@@ -1,5 +1,4 @@
 "use strict";
-// write a function to create a users table on the database
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -14,11 +13,23 @@ const pg_1 = require("pg");
 const client = new pg_1.Client({
     connectionString: "postgresql://prathamtamrakar:npg_dQOxMhnN2aE3@ep-weathered-block-a12fcqj4-pooler.ap-southeast-1.aws.neon.tech/test?sslmode=require"
 });
-client.connect().then(() => {
-    console.log('connected');
-}).catch((err) => {
-    console.log(`Error: ${err}`);
-});
+function connectToDatabase() {
+    return __awaiter(this, void 0, void 0, function* () {
+        try {
+            yield client.connect();
+            console.log("Connected to database");
+        }
+        catch (error) {
+            console.error("Error connecting to database:", error);
+        }
+    });
+}
+// client.connect().then(() => {
+//     console.log('connected');
+// }).catch((err) => {
+//     console.log(`Error: ${err}`);
+// })
+// write a function to create a users table on the database
 function createTable() {
     return __awaiter(this, void 0, void 0, function* () {
         const result = yield client.query(`
@@ -33,12 +44,105 @@ function createTable() {
     });
 }
 // createTable()
+// insert a new user into the database
 function insert() {
     return __awaiter(this, void 0, void 0, function* () {
         const result = yield client.query(`
         INSERT INTO users (username,email,password) VALUES ('rajJaiswal','rajjaiswal@gmail.com','raj password')
     `);
         console.log(result);
+        // this is an insecure way to store data in your table
+        //  when you expose this functionality eventually via http, someone can do SQL INJECTION to get access t0o your data/ dalete your data
     });
 }
-insert();
+// insert()
+// other way of writing
+function insertData() {
+    return __awaiter(this, void 0, void 0, function* () {
+        const client = new pg_1.Client({
+            connectionString: "postgresql://prathamtamrakar:npg_dQOxMhnN2aE3@ep-weathered-block-a12fcqj4-pooler.ap-southeast-1.aws.neon.tech/test?sslmode=require"
+        });
+        try {
+            yield client.connect();
+            const insertQuery = "INSERT INTO users (username,email,password) VALUES ('prathamTamrakar','prathamtamrakar@gmail.com','pratham password')";
+            const res = yield client.query(insertQuery);
+            console.log("insertion success", res);
+            // this is an insecure way to store data in your table
+            //  when you expose this functionality eventually via http, someone can do SQL INJECTION to get access t0o your data/ dalete your data
+        }
+        catch (error) {
+            console.error(error);
+        }
+        finally {
+            yield client.end();
+        }
+    });
+}
+function insertUser(username, email, password) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const insertQuery = " INSERT INTO users (username,email,password) VALUES ($1,$2,$3)";
+        const values = [username, email, password];
+        const result = yield client.query(insertQuery, values);
+        console.log(result);
+    });
+}
+// insertUser('prathamSharma','prathamsharma@gmail.com','pratham@123')
+function getUser(email) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const insertQuery = "SELECT * from users WHERE email = $1";
+        const values = [email];
+        const result = yield client.query(insertQuery, values);
+        if (result.rows.length > 0) {
+            console.log('User Found', result.rows[0]);
+            return result.rows[0];
+        }
+        else {
+            console.log('No user found with the given email');
+            return null;
+        }
+    });
+}
+// get all users from the database
+function getAllUser() {
+    return __awaiter(this, void 0, void 0, function* () {
+        try {
+            const result = yield client.query(`
+      SELECT * FROM users
+    `);
+            console.log("Users:", result.rows);
+        }
+        catch (error) {
+            console.error("Error getting users:", error);
+        }
+    });
+}
+// Delete a user in the database
+function deleteUser(email) {
+    return __awaiter(this, void 0, void 0, function* () {
+        try {
+            const result = yield client.query(`
+      DELETE FROM users
+      WHERE email = $1
+      RETURNING *
+    `, [email]);
+            console.log("User deleted:", result.rows[0]);
+        }
+        catch (error) {
+            console.error("Error deleting user:", error);
+        }
+    });
+}
+connectToDatabase()
+    .then(() => __awaiter(void 0, void 0, void 0, function* () {
+    yield insertUser("anshu2", "anshu2@gmail.com", "12345"); // You can add more users here by calling this function multiple times with different values
+    // await insertUser('prathamSharma','prathamsharma@gmail.com','pratham@123')
+    // await getUser('anshu2@gmail.com')
+    // await getAllUser()
+    // await deleteUser('anshu2@gmail.com')
+    // await deleteUser('prathamsharma@gmail.com')
+}))
+    .catch((error) => {
+    console.error("Error connecting to database:", error);
+}).finally(() => __awaiter(void 0, void 0, void 0, function* () {
+    yield client.end();
+}));
--- Week-10/programs/10.1-postgresSQL/src/index.ts
+++ Week-10/programs/10.1-postgresSQL/src/index.ts
@@ -1,4 +1,3 @@
-// write a function to create a users table on the database
 
 import { Client } from "pg";
 
@@ -8,20 +7,20 @@ const client = new Client({
 
 async function connectToDatabase() {
     try {
-      await client.connect();
-      console.log("Connected to database");
+        await client.connect();
+        console.log("Connected to database");
     } catch (error) {
-      console.error("Error connecting to database:", error);
+        console.error("Error connecting to database:", error);
     }
 }
 
-client.connect().then(()=>{
-    console.log('connected');
-}).catch((err)=>{
-    console.log(`Error: ${err}`); 
-})
+// client.connect().then(() => {
+//     console.log('connected');
+// }).catch((err) => {
+//     console.log(`Error: ${err}`);
+// })
 
-
+// write a function to create a users table on the database
 async function createTable() {
     const result = await client.query(`
         CREATE TABLE users (
@@ -36,7 +35,7 @@ async function createTable() {
 // createTable()
 
 // insert a new user into the database
-async function insert(){
+async function insert() {
     const result = await client.query(`
         INSERT INTO users (username,email,password) VALUES ('rajJaiswal','rajjaiswal@gmail.com','raj password')
     `)
@@ -44,4 +43,93 @@ async function insert(){
     // this is an insecure way to store data in your table
     //  when you expose this functionality eventually via http, someone can do SQL INJECTION to get access t0o your data/ dalete your data
 }
-insert()
+// insert()
+
+// other way of writing
+async function insertData() {
+    const client = new Client({
+        connectionString: "postgresql://prathamtamrakar:npg_dQOxMhnN2aE3@ep-weathered-block-a12fcqj4-pooler.ap-southeast-1.aws.neon.tech/test?sslmode=require"
+    })
+    try {
+        await client.connect();
+        const insertQuery = "INSERT INTO users (username,email,password) VALUES ('prathamTamrakar','prathamtamrakar@gmail.com','pratham password')"
+        const res = await client.query(insertQuery)
+        console.log("insertion success", res);
+        // this is an insecure way to store data in your table
+        //  when you expose this functionality eventually via http, someone can do SQL INJECTION to get access t0o your data/ dalete your data
+
+    } catch (error) {
+        console.error(error);
+    } finally {
+        await client.end()
+    }
+}
+
+async function insertUser(username: string,email:string,password:string){
+    const insertQuery = " INSERT INTO users (username,email,password) VALUES ($1,$2,$3)"
+    const values = [username,email,password]
+    const result  = await client.query(insertQuery,values);
+    console.log(result);
+}
+
+// insertUser('prathamSharma','prathamsharma@gmail.com','pratham@123')
+
+async function getUser(email:string){
+    const insertQuery = "SELECT * from users WHERE email = $1"
+    const values = [email]
+    const result = await client.query(insertQuery,values);
+
+    if(result.rows.length > 0){
+        console.log('User Found',result.rows[0]);
+        return result.rows[0]
+    } else {
+        console.log('No user found with the given email');
+        return null
+    }
+}
+
+// get all users from the database
+async function getAllUser() {
+  try {
+    const result = await client.query(`
+      SELECT * FROM users
+    `);
+
+    console.log("Users:", result.rows);
+  } catch (error) {
+    console.error("Error getting users:", error);
+  }
+}
+
+// Delete a user in the database
+async function deleteUser(email: string) {
+  try {
+    const result = await client.query(`
+      DELETE FROM users
+      WHERE email = $1
+      RETURNING *
+    `, [email]);
+
+    console.log("User deleted:", result.rows[0]);
+  } catch (error) {
+    console.error("Error deleting user:", error);
+  }
+}
+
+connectToDatabase()
+  .then(async () => {
+    await insertUser("anshu2", "anshu2@gmail.com", "12345")    // You can add more users here by calling this function multiple times with different values
+
+    // await insertUser('prathamSharma','prathamsharma@gmail.com','pratham@123')
+
+    // await getUser('anshu2@gmail.com')
+    // await getAllUser()
+    // await deleteUser('anshu2@gmail.com')
+    // await deleteUser('prathamsharma@gmail.com')
+
+  }) 
+  .catch((error) => {
+    console.error("Error connecting to database:", error);
+  }).finally(async() => { 
+    await client.end();
+  })
